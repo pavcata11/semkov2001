1731eaffed3833ed65ebc40776af9681ab9abc9c
Pavel <Pavel@DESKTOP-QOLTGOV>
2023-05-08 15:40:09 +0300

    corect unit test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,7 +5,7 @@ describe('sample test', () => {
 
   it('displays the resources text', () => {
     cy.get('h1')
-    .contains('Welcome to my app!');
+    .contains('Welcome to my app pavel111111111111111111111111111!');
   })
   it('renders the Netlify logo image', () => {
     cy.get('img')
