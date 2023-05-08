5604c0b99f5304fc93a8a49abd9de5779ca46ed2
Pavel <Pavel@DESKTOP-QOLTGOV>
2023-05-08 15:30:08 +0300

    Add Themplate Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Navbar from '@components/NavBar'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Отчитане на активност Semkov2001</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
+        <NavBar></NavBar>
         <Header title="Welcome to my app pavel111111111111111111111111111!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
